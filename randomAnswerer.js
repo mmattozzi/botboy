6657d701d485e5d7eb1b75c77a41ab0ce7fac629
Mike Mattozzi <michael_mattozzi@comcast.com>
2014-01-13 19:02:01 -0500

    Handle trailing newlines appropriately

--- randomAnswerer.js
+++ randomAnswerer.js
@@ -2,12 +2,17 @@ var fs = require('fs');
 
 function addFileBasedRandomAnswerer(bot, pattern, file) {
     
-    var macesResponses = fs.readFileSync(file, 'utf8').split('\n');
+    var source = fs.readFileSync(file, 'utf8').split('\n');
+
+    // Remove any empty entry caused by trailing newline
+    if (source[source.length - 1] === '') {
+    	source.pop();
+    }
     
     bot.addMessageListener("random " + pattern, function(nick, message) {
 		var re = new RegExp(pattern);
         if (re.test(message)) {
-			bot.say(macesResponses[Math.floor(Math.random()*macesResponses.length)]);
+			bot.say(source[Math.floor(Math.random()*source.length)]);
 			return false;
 		}
 		return true;
